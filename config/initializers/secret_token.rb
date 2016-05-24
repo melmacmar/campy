80ea4542063ab1145ad918d325ecd4dfc3f04dae
Melanie Marshall <melmacmar@gmail.com>
2016-05-24 10:09:59 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Campy::Application.config.secret_key_base = 'ea78a33e1a715b7b4901fe629c474d94db78e660cd3b95cd9031cf33c1fbf4aee08a8f5e962202870bafb4fcbcbf869551d5f195172c4d31b4f25bc798c8d61a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Campy::Application.config.session_store :cookie_store, key: '_campy_session'
